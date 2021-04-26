d7e48f72057fb98289350057e314d34b062966a3
Viraj Patel <virajpatel@Virajs-MacBook-Pro.local>
2021-04-25 18:01:17 -0700

    wip: keyword value

--- models/post.js
+++ models/post.js
@@ -14,6 +14,10 @@ const postSchema = new Schema({
         type: String,
         required: true,
     },
+    keywords: {
+        type: String,
+        required: true,
+    }
 }, { timestamps: true });
 
 
